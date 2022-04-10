4cbc64bc3459a09353805819788e720fe65d205a
capricieuxV <capricieuxyue@gmail.com>
2022-04-09 22:29:19 -0400

    change back to our group

--- src/IR_sensor.cpp
+++ src/IR_sensor.cpp
@@ -6,8 +6,8 @@ void IR_sensor::init(){
 
 float IR_sensor::ReadData(){ // distance in cm
     adc_value = analogRead(irPin);
-    // return 1000 / ((adc_value*0.21) - 7.28);
-    return 1000/(0.23*adc_value-7.5); //group 1
+    return 1000 / ((adc_value*0.21) - 7.28);
+    // return 1000/(0.23*adc_value-7.5); //group 1
 }
 
 void IR_sensor::PrintData(){
